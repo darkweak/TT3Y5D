03327251aa0c903eaf3cbc6bd339cc27b90651c7
Darkweak <sylvain@les-tilleuls.coop>
2021-07-13 01:09:51 +0200

    Update

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -21,6 +21,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 	fi
 
+  bin/console ca:cl
 	echo "Waiting for db to be ready..."
 	until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
 		sleep 1
